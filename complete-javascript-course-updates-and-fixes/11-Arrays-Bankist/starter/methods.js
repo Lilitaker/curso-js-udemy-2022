3ca15a7c25de6258bc81ab0ad8aef65fad85c861
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-04-12 11:21:44 -0500

    Add find method in methods file

--- complete-javascript-course-updates-and-fixes/11-Arrays-Bankist/starter/methods.js
+++ complete-javascript-course-updates-and-fixes/11-Arrays-Bankist/starter/methods.js
@@ -148,3 +148,48 @@ const eurToUsd2 = 1.1;
 const totalDepositsUSD = movements3.filter(mov => mov > 0).map(mov => mov * eurToUsd2).reduce((acc, mov) => acc + mov, 0);
 
 console.log(Math.trunc(totalDepositsUSD)); //5522
+
+
+console.log('FIND() METHOD');
+
+const firstWithdrawal = movements3.find(mov => mov < 0);
+console.log(firstWithdrawal); //-400
+
+const accountA = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const accountB = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const accountC = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const accountD = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts1 = [accountA, accountB, accountC, accountD];
+
+const account = accounts1.find(acc => acc.owner === 'Jessica Davis');
+console.log(account); //{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222}
+
+//for...of version
+for(const account of accounts1){
+  if (account.owner === 'Jessica Davis') console.log(account);
+}
+//{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222}
