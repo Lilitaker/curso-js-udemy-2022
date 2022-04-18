fb66f3603b79aab80801b825f60ec8380013db76
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-04-18 11:52:06 -0500

    Add close account functionality

--- complete-javascript-course-updates-and-fixes/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-updates-and-fixes/11-Arrays-Bankist/starter/script.js
@@ -29,7 +29,14 @@ const account4 = {
   pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4];
+const account5 = {
+  owner: 'Liliana Vasquez',
+  movements: [500, -1000, 1250, -50, 900, 450],
+  interestRate: 0.5,
+  pin: 5555,
+};
+
+const accounts = [account1, account2, account3, account4, account5];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -81,21 +88,29 @@ const displayMovements = function (movements) {
 // Calculate balance of the account (step 3)
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 // Calculate incomes, outgoings and interests (step 4)
-const calcDisplaySummary = function(acc){
-  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, dep) => acc + dep, 0);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, dep) => acc + dep, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const outgoing = Math.abs(acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0));
+  const outgoing = Math.abs(
+    acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
+  );
   labelSumOut.textContent = `${outgoing}€`;
 
   // The bank pays out an interest each time that there's a deposit. It only pays an interest if it is at least one euro
-  const interest = acc.movements.filter(mov => mov > 0).map(deposit => deposit * acc.interestRate / 100).filter((int) => int >= 1).reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`
-}
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
 
 // Create usernames for login (step 2)
 const createUsernames = function (accs) {
@@ -110,33 +125,37 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
-//Update UI (step 7...refactored)
-const updateUI = function(acc){
-   //Display movements
-   displayMovements(acc.movements);
+// Update UI (step 7...refactored)
+const updateUI = function (acc) {
+  //Display movements
+  displayMovements(acc.movements);
 
-   //Display balance
-   calcDisplayBalance(acc);
+  //Display balance
+  calcDisplayBalance(acc);
 
-   //Display summary
-   calcDisplaySummary(acc);
-}
+  //Display summary
+  calcDisplaySummary(acc);
+};
 
 // Event handler for the login button (step 5)
 
 let currentAccount;
 
-btnLogin.addEventListener('click', function(e){
+btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
 
   //Input user
-  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
 
-  //Input PIN 
-  if(currentAccount?.pin === Number(inputLoginPin.value)){
+  //Input PIN
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
     //Display message and UI
-    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}!`;
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }!`;
     containerApp.style.opacity = 100;
 
     //Clear input fields
@@ -149,24 +168,57 @@ btnLogin.addEventListener('click', function(e){
 });
 
 // Event handler for transfering (step 6)
-btnTransfer.addEventListener('click', function(e){
+btnTransfer.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
 
-  //Get the value of transfer inputs 
+  //Get the value of transfer inputs
   const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
 
   //Clear input fields
   inputTransferTo.value = inputTransferAmount.value = '';
 
   //Conditions to send the money
-  if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc.username !== currentAccount.username){
-    //Doing the transfer: Add to the giver the outgoing and to receiver the income 
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc.username !== currentAccount.username
+  ) {
+    //Doing the transfer: Add to the giver the outgoing and to receiver the income
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
     //Call the function to update UI
     updateUI(currentAccount);
   }
-})
+});
+
+// Close account (step 8)
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  //Clear input fields
+  inputCloseUsername.value = inputClosePin.value = '';
+
+  //Setting start login message
+  labelWelcome.textContent = 'Log in to get started';
+});
